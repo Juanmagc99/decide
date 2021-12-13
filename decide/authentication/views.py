c2166951cd54e5a46dcdf021a8081a48059568f4
danricost <danricost@alum.us.es>
2021-12-13 20:27:20 +0100

    Añadido la verificacion por correo tras el registro de usuario

--- decide/authentication/views.py
+++ decide/authentication/views.py
@@ -14,6 +14,51 @@ from django.core.exceptions import ObjectDoesNotExist
 from .serializers import UserSerializer
 
 
+
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.sites.shortcuts import get_current_site
+from django.contrib import messages
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_text, force_str
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth import get_user_model
+UserModel = get_user_model()
+from django.core.mail import send_mail
+from .forms import RegisterUserForm
+
+
+
+
+
+def activate(request, uidb64, token):
+
+    # uid = urlsafe_base64_decode(uidb64.encode('utf-8').strip()).encode('utf-8').strip().decode('cp1252')
+    # user = UserModel._default_manager.get(pk=uid)
+
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = UserModel._default_manager.get(pk=uid)
+
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        # Redirect user to login
+        messages.success(request, 'Successful email confirmation, you can proceed to login.')
+        return HttpResponseRedirect(reverse('login'))
+    else:
+        return HttpResponse('Activation link is invalid!')
+
+
+
+
+
+
 class GetUserView(APIView):
     def post(self, request):
         key = request.data.get('token', '')
