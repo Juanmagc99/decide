58b188865c72b227107019e335af78557bbf1c77
danricost <danricost@alum.us.es>
2021-12-13 21:00:00 +0100

    Añadidas vistas de login y logout

--- decide/authentication/urls.py
+++ decide/authentication/urls.py
@@ -3,14 +3,11 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 from .views import GetUserView, LogoutView, RegisterView
 
-from django.contrib.auth.views import login, logout
-
 urlpatterns = [
     path('login/', obtain_auth_token),
     path('logout/', LogoutView.as_view()),
     path('getuser/', GetUserView.as_view()),
     path('register/', RegisterView.as_view()),
-
-    path('loginUser/', login, {'template_name': 'login.html'}, name='loginUser'),
-    path('logoutUser/', logout, name="logoutUser"),
+    
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
