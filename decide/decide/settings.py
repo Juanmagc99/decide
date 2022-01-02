59a39382cb0e14b62e104e7258dcaf020f5b8406
Daniel Rico Ostos <72860383+danricost@users.noreply.github.com>
2022-01-02 21:16:07 +0100

    Update settings.py

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -118,6 +118,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
